2216464eaa5a9aa36f3ff2a03e9b74c688dee21e
AmanVerma5 <amanv4775@gmail.com>
2024-01-11 16:27:29 +0530

    1

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+class Shape {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  
+    moveTo(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+  
+  class Circle extends Shape {
+    constructor(x, y, radius) {
+      super(x, y);
+      this.radius = radius;
+    }
+  
+    moveTo(x, y) {
+      super.moveTo(x, y);
+    }
+  }
+  
+  // Test case
+  const myCircle = new Circle(0, 0, 5);
+  console.log(myCircle.x, myCircle.y); // Output: 0, 0
+  myCircle.moveTo(10, 20);
+  console.log(myCircle.x, myCircle.y); // Output: 10, 20
